39f4b11011b4ab034700fb6aaaf20c3ceb18645c
Annymea <anna.grahl@gmx.de>
2025-06-05 22:04:17 +0200

    finish first gin tutorial

--- web-services-gin/main.go
+++ web-services-gin/main.go
@@ -35,11 +35,25 @@ func postToDo(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newToDo)
 }
 
+func getListItemById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range toDoList {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "item not found"})
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/list", getToDoList)
 	router.POST("/todo", postToDo)
+	router.GET("/todo/:id", getListItemById)
 
 	router.Run("localhost:8080")
 }
