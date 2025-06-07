9c217f61a57e9985cbb1fbc1b0f6a7ab750c711e
Annymea <anna.grahl@gmx.de>
2025-06-07 11:58:47 +0200

    refactor: restructure some code

--- internal/handler/toDo_handler.go
+++ internal/handler/toDo_handler.go
@@ -1,11 +1,11 @@
-package router
+package handler
 
 import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 
-	"ToDoInventory/models"
+	"ToDoInventory/internal/models"
 )
 
 var toDoList = []models.ToDo{
@@ -14,11 +14,11 @@ var toDoList = []models.ToDo{
 	{ID: "3", Title: "API testen", Done: false},
 }
 
-func getToDoList(c *gin.Context) {
+func GetToDoList(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, toDoList)
 }
 
-func postToDo(c *gin.Context) {
+func PostToDo(c *gin.Context) {
 	var newToDo models.ToDo
 
 	err := c.BindJSON(&newToDo)
@@ -31,7 +31,7 @@ func postToDo(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newToDo)
 }
 
-func getListItemById(c *gin.Context) {
+func GetListItemById(c *gin.Context) {
 	id := c.Param("id")
 
 	for _, a := range toDoList {
@@ -42,16 +42,4 @@ func getListItemById(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "item not found"})
-}
-
-func InitRouter() *gin.Engine {
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.Use(gin.Recovery()) //ToDo: Was ist das?
-
-	router.GET("/list", getToDoList)
-	router.POST("/todo", postToDo)
-	router.GET("/todo/:id", getListItemById)
-
-	return router
 }
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "ToDoInventory/router"
-
-func main() {
-	router := router.InitRouter()
-
-	router.Run("localhost:8080")
-}
