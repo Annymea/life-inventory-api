bd8121aa1451bf7d7d0808812203ca6eba39299c
Annymea <anna.grahl@gmx.de>
2025-06-09 18:42:41 +0200

    connect to docker database and create table

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"ToDoInventory/internal/models"
 	"ToDoInventory/internal/router"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	//router stuff
 	gin.ForceConsoleColor()
 	r := gin.Default() //Default Middleware -> Wenn ich hier was eigenes habe, dann muss ich es anpassen
 
 	router.RegisterRoutes(r)
 
+	//database stuff
+	dbUrl := "postgres://pg:pass@localhost:5432/toDoList"
+	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	db.AutoMigrate(&models.ToDo{})
+
+	//start server
 	r.Run("localhost:8080")
 }
--- internal/models/toDo.go
+++ internal/models/toDo.go
@@ -1,7 +1,8 @@
 package models
 
 type ToDo struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
-	Done  bool   `json:"done"`
+	ID          string `json:"id" gorm:"primaryKey"`
+	Title       string `json:"title"`
+	Done        bool   `json:"done"`
+	PlannedDate string `json:"plannedDate"`
 }
