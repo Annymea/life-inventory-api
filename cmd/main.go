83e93fa51a2ba6df5a2ea7018657e80081d93fa4
Annymea <anna.grahl@gmx.de>
2025-06-10 09:40:55 +0200

    refactor: move db stuff in own folder

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,26 @@
 package main
 
 import (
-	"ToDoInventory/internal/models"
 	"ToDoInventory/internal/router"
-	"log"
+	"ToDoInventory/internal/storage"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
+
+	//database stuff
+	db := storage.InitDb()
+
+	if db == nil {
+		return
+	}
+
 	//router stuff
 	gin.ForceConsoleColor()
 	r := gin.Default() //Default Middleware -> Wenn ich hier was eigenes habe, dann muss ich es anpassen
 
 	router.RegisterRoutes(r)
-
-	//database stuff
-	dbUrl := "postgres://pg:pass@localhost:5432/toDoList"
-	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-	db.AutoMigrate(&models.ToDo{})
-
-	testToDo := &models.ToDo{ID: "4", Title: "Test", Done: true, PlannedDate: ""}
-	db.Create(&testToDo)
 
 	//start server
 	r.Run("localhost:8080")
