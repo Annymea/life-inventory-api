151a4fcf25ced062967cf5ef31ad46e5f5188aec
Annymea <anna.grahl@gmx.de>
2025-06-10 19:54:35 +0200

    add connection to db in handler

--- cmd/main.go
+++ cmd/main.go
@@ -20,7 +20,7 @@ func main() {
 	gin.ForceConsoleColor()
 	r := gin.Default() //Default Middleware -> Wenn ich hier was eigenes habe, dann muss ich es anpassen
 
-	router.RegisterRoutes(r)
+	router.RegisterRoutes(r, db)
 
 	//start server
 	r.Run("localhost:8080")
