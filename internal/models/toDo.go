78d3e69b39d3052e045bb3ae7c2beea77fb00ac9
Annymea <anna.grahl@gmx.de>
2025-06-14 21:41:20 +0200

    remove unsused type

--- internal/models/toDo.go
+++ internal/models/toDo.go
@@ -6,10 +6,3 @@ type ToDoDTO struct {
 	Done        bool   `json:"done"`
 	PlannedDate string `json:"plannedDate"`
 }
-
-type PostToDoRequest struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Done        bool   `json:"done"`
-	PlannedDate string `json:"plannedDate"`
-}
