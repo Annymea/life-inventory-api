196c4d1f3bc8f0b4560bfa69ffdef4dd266a01fc
Anna <anna.grahl@gmx.de>
2025-06-16 10:51:48 +0200

    add first request to swagger

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
+// @title           ToDo Inventory API
+// @version         1.0
+// @description     API zur Verwaltung von ToDos.
+// @host            localhost:8080
+// @BasePath        /
 package main
 
 import (
+	_ "ToDoInventory/docs"
 	"ToDoInventory/internal/router"
 	"ToDoInventory/internal/storage"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -21,7 +29,7 @@ func main() {
 	r := gin.Default() //Default Middleware -> Wenn ich hier was eigenes habe, dann muss ich es anpassen
 
 	router.RegisterRoutes(r, db)
-
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	//start server
 	r.Run("localhost:8080")
 }
