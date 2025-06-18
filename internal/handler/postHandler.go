b3815c37d6047c9909f9406dbdbd573ae3b93400
Annymea <anna.grahl@gmx.de>
2025-06-18 20:45:00 +0200

    add swagger comments to post request

--- internal/handler/postHandler.go
+++ internal/handler/postHandler.go
@@ -9,17 +9,13 @@ import (
 	"ToDoInventory/internal/service"
 )
 
-// PostToDo erstellt einen neuen ToDo-Eintrag
-// @Summary Neues ToDo anlegen
-// @Description Erstellt ein neues ToDo
-// @Tags todos
-// @Accept json
-// @Produce json
-// @Param todo body models.ToDoDTO true "Neues ToDo"
+// @Summary	Create new todo
+// @Description Creates a new todo
+// @Param todo body models.ToDoDTO true "New todo"
 // @Success 201 {object} models.ToDoDTO
 // @Failure 400
 // @Failure 500
-// @Router /todos [post]
+// @Router /todo [post]
 func (h *Handler) PostToDo(c *gin.Context) {
 	var newToDo models.ToDoDTO
 
