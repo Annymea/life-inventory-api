e65b33cc13c1597b1561132c5c36c3bcd44f2826
Annymea <anna.grahl@gmx.de>
2025-06-19 11:09:50 +0200

    rename services

--- internal/service/service.go
+++ internal/service/service.go
@@ -7,31 +7,31 @@ import (
 
 //Hier wird das mapping von den entgegengenommenen Types zu Datenbanktypen vorgenommen
 
-func ConvToToDoDTO(dbToDo datatypes.Entry) models.EntryDto {
+func ConvToEntryDto(entryDto datatypes.Entry) models.EntryDto {
 	return models.EntryDto{
-		PlannedDate: dbToDo.PlannedDate,
-		Done:        dbToDo.Done,
-		Title:       dbToDo.Title,
-		ID:          dbToDo.ID,
+		PlannedDate: entryDto.PlannedDate,
+		Done:        entryDto.Done,
+		Title:       entryDto.Title,
+		ID:          entryDto.ID,
 	}
 }
 
-func ConvListToToDoDTO(dbToDoList []datatypes.Entry) []models.EntryDto {
+func ConvListToEntryDto(dbEntryList []datatypes.Entry) []models.EntryDto {
 	convertedList := []models.EntryDto{}
 
-	for _, toDo := range dbToDoList {
-		resp := ConvToToDoDTO(toDo)
+	for _, entry := range dbEntryList {
+		resp := ConvToEntryDto(entry)
 		convertedList = append(convertedList, resp)
 	}
 
 	return convertedList
 }
 
-func ConvToDatabaseToDo(dtoToDo models.EntryDto) datatypes.Entry {
+func ConvToDbEntry(entryDto models.EntryDto) datatypes.Entry {
 	return datatypes.Entry{
-		ID:          dtoToDo.ID,
-		Title:       dtoToDo.Title,
-		Done:        dtoToDo.Done,
-		PlannedDate: dtoToDo.PlannedDate,
+		ID:          entryDto.ID,
+		Title:       entryDto.Title,
+		Done:        entryDto.Done,
+		PlannedDate: entryDto.PlannedDate,
 	}
 }
