fd32f32b3c13a3b2b119be0acc44631241dbe1f6
Annymea <anna.grahl@gmx.de>
2025-06-21 20:52:54 +0200

    autocreate id when creating item

--- internal/storage/datatypes/entry.go
+++ internal/storage/datatypes/entry.go
@@ -1,8 +1,18 @@
 package datatypes
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Entry struct {
-	ID          string `gorm:"primaryKey"`
+	ID          string `gorm:"primaryKey;unique"`
 	Title       string
 	Done        bool
 	PlannedDate string
+}
+
+func (e *Entry) BeforeCreate(tx *gorm.DB) (err error) {
+	e.ID = uuid.New().String()
+	return
 }
