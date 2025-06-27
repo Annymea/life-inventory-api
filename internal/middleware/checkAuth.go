8055a94df646a47bdecb5a84612d453e50dfdb6f
Anna <anna.grahl@gmx.de>
2025-06-28 01:19:47 +0200

    implement usage of middleware

--- internal/middleware/checkAuth.go
+++ internal/middleware/checkAuth.go
@@ -13,68 +13,68 @@ import (
 	"gorm.io/gorm"
 )
 
-func CheckAuth(c *gin.Context, db *gorm.DB) {
+func CheckAuth(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
 
-	authHeader := c.GetHeader("Authorization")
-
-	//Im header muss hier das auth token mit dabei sein
-	if authHeader == "" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	//liest den header aus und schaut, ob es das richtige format hat
-	authToken := strings.Split(authHeader, " ")
-	if len(authToken) != 2 || authToken[0] != "Bearer" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	//liest den token und checkt den auf richtige signatur und auf gültigkeit
-	tokenString := authToken[1]
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		//Im header muss hier das auth token mit dabei sein
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
 		}
-		return []byte(os.Getenv("SECRET")), nil
-	})
-	if err != nil || !token.Valid {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
+
+		//liest den header aus und schaut, ob es das richtige format hat
+		authToken := strings.Split(authHeader, " ")
+		if len(authToken) != 2 || authToken[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		//liest den token und checkt den auf richtige signatur und auf gültigkeit
+		tokenString := authToken[1]
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(os.Getenv("SECRET")), nil
+		})
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		//claims sind inhalte, die im token gespeichert werden
+		//mit mapClaims werden die daten zugreifbar gemacht
+		claims, ok := token.Claims.(jwt.MapClaims)
+		//ok ist die typüberprüfung
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		//checkt ob das token noch nicht abgelaufen ist
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "token expired"})
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		//checkt ob der user noch in der datenbank existiert - falls der user gelöscht wurde, das token aber noch nicht abgelaufen ist
+		var user datatypes.User
+		db.Where("ID=?", claims["id"]).Find(&user)
+
+		if user.ID == 0 {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		//current user wird im kontext gesetzt, um die nächsten requests abfertigen zu lassen
+		c.Set("currentUser", user)
+
+		c.Next()
 	}
-
-	//claims sind inhalte, die im token gespeichert werden
-	//mit mapClaims werden die daten zugreifbar gemacht
-	claims, ok := token.Claims.(jwt.MapClaims)
-	//ok ist die typüberprüfung
-	if !ok {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-		c.Abort()
-		return
-	}
-
-	//checkt ob das token noch nicht abgelaufen ist
-	if float64(time.Now().Unix()) > claims["exp"].(float64) {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "token expired"})
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	//checkt ob der user noch in der datenbank existiert - falls der user gelöscht wurde, das token aber noch nicht abgelaufen ist
-	var user datatypes.User
-	db.Where("ID=?", claims["id"]).Find(&user)
-
-	if user.ID == 0 {
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	//current user wird im kontext gesetzt, um die nächsten requests abfertigen zu lassen
-	c.Set("currentUser", user)
-
-	c.Next()
-
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,7 +1,9 @@
 package router
 
 import (
+	"LifeInventoryApi/internal/handler/authhandler"
 	"LifeInventoryApi/internal/handler/entryhandler"
+	"LifeInventoryApi/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -13,18 +15,28 @@ import (
 func RegisterRoutes(engine *gin.Engine, db *gorm.DB) {
 
 	entryHandlerDI := entryhandler.NewEntryHandler(db)
+	authHandlerDI := authhandler.NewAuthHandler(db)
 
 	engine.GET("/docu/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
+	auth := engine.Group("/auth")
+	{
+		auth.POST("/auth/signup", authHandlerDI.CreateUser)
+		auth.POST("/auth/login", authHandlerDI.Login)
+	}
+
 	api := engine.Group("/api")
 	{
-		v1 := api.Group("/v1")
-		v1.GET("/list", entryHandlerDI.GetEntryList)
-		v1.POST("/entry", entryHandlerDI.PostEntry)
-		v1.GET("/entry/:id", entryHandlerDI.GetListItemById)
-		v1.GET("/entry", entryHandlerDI.GetEntryListByParameters)
-		v1.DELETE("/entry/:id", entryHandlerDI.DeleteEntryById)
-		v1.PUT("/entry", entryHandlerDI.UpdateEntry)
+		//middleware wird nur für die folgenden routen gebraucht. bei den anderern holt man sich ja erst das token
+		v1 := api.Group("/v1", middleware.CheckAuth(db))
+		{
+			v1.GET("/list", entryHandlerDI.GetEntryList)
+			v1.POST("/entry", entryHandlerDI.PostEntry)
+			v1.GET("/entry/:id", entryHandlerDI.GetListItemById)
+			v1.GET("/entry", entryHandlerDI.GetEntryListByParameters)
+			v1.DELETE("/entry/:id", entryHandlerDI.DeleteEntryById)
+			v1.PUT("/entry", entryHandlerDI.UpdateEntry)
+		}
 	}
 
 }
